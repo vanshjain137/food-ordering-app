4d91b923c31b4983bac29b4ebe3c7528d295eddf
Vansh Jain <vanshjain181@gmail.com>
2024-05-07 11:54:40 +0530

    home page

---
+++ src/app/components/menu/MenuItem.js
@@ -0,0 +1,16 @@
+    export default function MenuItem(){
+        return(
+            <div className="bg-gray-200 p-4 rounded-lg text-center group hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all">
+                <div className="text-center">
+                <img src="/pizza.png" className="max-h-auto max-h-24 block mx-auto" alt="pizza"/>
+                </div>
+                    
+                    <h4 className="font-semibold text-xl my-3">Veg Pizza</h4>
+                    <p className="text-gray-500 text-sm">
+                        Lorem ipsum dolor sit amet, consectetur adipisicingelit
+                    </p>
+                    <button className="mt-4 bg-primary text-white rounded-full px-8 py-2">Add to cart ₹250</button>
+I
+                </div>
+        );
+    }
